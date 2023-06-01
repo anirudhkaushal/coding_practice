5a5ece81dbaf75ec53d4c2b7ee8454331e984df4
Anirudh Kaushal <anirudhkushal30@gmail.com>
2023-05-31 20:58:58 -0400

    added q2 move negative in front optimal approaches code

--- two-pointer/q2_moveNegativeInfront.py
+++ two-pointer/q2_moveNegativeInfront.py
@@ -21,5 +21,49 @@ def separateNegativeAndPositive(nums):
 
     return nums
 
+# two pointer approach - one pointer at first element; other pointer at last element
+def separateNegativeAndPositive_(nums):
+
+    n = len(nums)
+    i = 0
+    j = n - 1
+
+    while i < j:
+
+        if nums[i] < 0 and nums[j] >= 0:
+            i += 1
+            j -= 1
+        elif nums[i] >= 0 and nums[j] < 0:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j -= 1
+        elif nums[i] < 0 and nums[j] < 0:
+            i += 1
+        else:
+            j -= 1
+
+    return nums 
+
+# partition approach - which is used in quick sort
+def separateNegativeAndPositive__(nums):
+
+    n = len(nums)
+    i = -1
+    pivot = 0
+
+    for j in range(n):
+
+        if nums[j] < pivot:
+            i += 1
+            nums[i], nums[j] = nums[j], nums[i]
+
+    return nums
+            
+
+
+
+
+
+
 
 
