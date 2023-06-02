683bb04d0b9f592fcbd3c9a541ebb163a5a37e83
Anirudh Kaushal <anirudhkushal30@gmail.com>
2023-06-01 23:02:12 -0400

    added q2 optimal approach code

--- mixedQues-basic-dsa/q2_intersectionOfTwoArrays.py
+++ mixedQues-basic-dsa/q2_intersectionOfTwoArrays.py
@@ -6,7 +6,7 @@ from math import *
 # brute force approach: 
 # time complexity : almost O(n*m) 
 # space complexity: O(m)
-def findArrayIntersection(arr: list, n: int, brr: list, m: int):
+def findArrayIntersection_(arr: list, n: int, brr: list, m: int):
     
     vis = [0]*m
     ans = []
@@ -24,3 +24,25 @@ def findArrayIntersection(arr: list, n: int, brr: list, m: int):
 
     return ans
         
+# optimal approach
+# time complexity: in worst case O(n + m)
+# space complexity: O(1)
+def findArrayIntersection(arr: list, n: int, brr: list, m: int):
+
+    i = 0
+    j = 0
+    ans = []
+
+    while i < n and j < m:
+        if arr[i] < brr[j]:
+            i += 1
+        elif arr[i] > brr[j]:
+            j += 1
+        else:
+            ans.append(arr[i])
+            i += 1
+            j += 1
+
+    return ans
+
+
