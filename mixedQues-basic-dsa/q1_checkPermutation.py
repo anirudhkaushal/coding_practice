3f81a387ecdb1b991ca3b4972701f137a3330d96
Anirudh Kaushal <anirudhkushal30@gmail.com>
2023-06-07 20:13:59 -0400

    added q1 optimal approach code

--- mixedQues-basic-dsa/q1_checkPermutation.py
+++ mixedQues-basic-dsa/q1_checkPermutation.py
@@ -3,8 +3,8 @@ from sys import *
 from collections import *
 from math import *
 
-# my approach - using hashmap/dictionary
-def areAnagram(str1, str2):
+# my approach - using 2 hashmap/dictionary; one for each str
+def areAnagram_(str1, str2):
     # Write your code here.
 
     ans = 0
@@ -31,3 +31,30 @@ def areAnagram(str1, str2):
             return 0
 
     return ans
+
+
+# optimal approach - using a single hashmap
+def areAnagram(str1, str2):
+
+    mpp = {}
+
+    if len(str1) != len(str2):
+        return 0
+
+    for ch in str1:
+        if ch in mpp:
+            mpp[ch] += 1
+        else:
+            mpp[ch] = 1
+
+    for ch in str2:
+        if ch in mpp:
+            mpp[ch] -= 1
+
+            if mpp[ch] < 0:
+                return 0
+        else:
+            return 0
+
+    return 1
+
